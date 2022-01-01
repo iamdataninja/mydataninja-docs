29e0ea5df7022c408cd6e66ec322b83fa1f2a309
Malkhazi Dartsmelidze <34204206+malkhazidartsmelidze@users.noreply.github.com>
2022-01-01 19:45:15 +0400

    finished chaning meta picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <Layout
       title={`Documentation - ${siteConfig.title}`}
-      description='Description will go into a meta tag in <head />'
+      description='Complete Documentation of Mydataninja'
     >
       <HomepageHeader />
       <main>
