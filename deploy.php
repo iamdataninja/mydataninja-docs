ca4f4d2368dfda1700c0d5dc85e5062a7f586630
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2024-07-18 18:17:56 +0400

    added new deploy script

--- deploy.php
+++ deploy.php
@@ -1,4 +1,13 @@
 <?php
-$build_dir = __DIR__ . '/build';
 
-exec("scp -v -P2221 -r $build_dir/* docs.mydataninja.com@docs.mydataninja.com:/home/docs.mydataninja.com");
+$build_dir = __DIR__ . '/build';
+$dir_name = 'build';
+date_default_timezone_set('Asia/Tbilisi');
+
+$date = date('Y-m-d-H-i-s', strtotime('now'));
+$filename = "$date-deploy.zip";
+
+exec("rm ./$dir_name/*.zip");
+exec("cd $dir_name && zip -r $filename ./");
+exec("scp -v -P2221 -r $build_dir/$filename docs.mydataninja.com@docs.mydataninja.com:/home/docs.mydataninja.com");
+exec("ssh -p 2221 docs.mydataninja.com@docs.mydataninja.com 'unzip -o /home/docs.mydataninja.com/$filename -d /home/docs.mydataninja.com/'");
